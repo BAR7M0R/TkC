94a1df8664ff0453c78348933997d017cc43b7e4
Bartmor <bartmor.21@gmail.com>
2025-04-15 23:33:26 +0200

    uzupełnienie części raportu oraz usunięcie nadmiarowych plików

--- lab 2/zad_1.m
+++ lab 2/zad_1.m
@@ -1,13 +1,15 @@
 % Generacja sygnałów cyfrowych - cz.2
 
-clear; close all;
+clear; clc; close all;
 
 %% Dane przykładowe
-Rs=30;      %prędkość symbolowa
-fp=120;      %próbkowanie
-N_fft=4096; %rozmiar FFT
-M=5;      %ilość odstępów międzysymbolowych
-A=0.2; %współczynnik przekroczenia pasma
+Rs=15;      %prędkość symbolowa -> bit/s
+fp=120;      %próbkowanie -> Hz
+M=5;      %ilość odstępów międzysymbolowych -> bit 
+Ts = 1/Rs; % okres symbolowy -> s/bit
+N_fft=4096; %rozmiar FFT -> bezwymiarowe
+
+
 
 
 
@@ -15,55 +17,60 @@ A=0.2; %współczynnik przekroczenia pasma
 %% a) 	
 
 % określenie dziedziny czasu
-t=0:1/fp:M/Rs;
+t=0:1/fp:M/Rs; % -> s
+% określenie dziedziny częstotliwości
+f=0:fp/N_fft:fp-1/N_fft; % -> Hz
 
 % wyznaczenie współczynników filtru
-h_sinc=Rs/fp*sinc(Rs*(t-M/(2*Rs)));
-
+h_sinc=1*Rs/fp*sinc(Rs*(t-M/(2*Rs))); % -> V or A
+sym_wst_gl= t(15) - t(7); % -> s
 % wyświetlenie odpowiedzi impulsowej w dziedzinie czasu
 figure(1)
 stem(t,h_sinc)
 grid;
 title('Odpowiedź impulsowa')
-
-% określenie dziedziny częstotliwości
-f=0:fp/N_fft:fp-1/N_fft;
-
+xlabel('$\textsf{Czas, }s$','Interpreter','latex')
+%xlabel('$s$','Interpreter','latex')
+ylabel('$\textsf{Amplituda, }V\textsf{ lub }A$', 'Interpreter','latex')
 %wyznaczenie charakterystyki częstotliwościowej
-H_sinc=fft(h_sinc,N_fft);
-abs_H_sinc=abs(H_sinc);
+H_sinc=fft(h_sinc,N_fft); % -> V or A
+abs_H_sinc=abs(H_sinc); % -> V or A
 
 % wyświetlenie ch-ki amplitudowej - skala liniowa
 figure(2)
 plot(f,abs_H_sinc);
 grid
 title('Charakterystyka amplitudowa - skala liniowa')
-
+xlabel('$\textsf{Czestotliwosc, }Hz$','Interpreter','latex')
+ylabel('$\textsf{Amplituda, }V\textsf{ lub }A$', 'Interpreter','latex')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   UZUPEŁNIĆ O DECYBELOWĄ
 %   
-fdB=10*log10(f);
-abs_H_sincdB = 10*log10(abs_H_sinc);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+abs_H_sinc_dB = 20*log10(abs_H_sinc); % -> dB
 figure(3)
-plot(f,abs_H_sincdB);
+plot(f,abs_H_sinc_dB);
 xlim ([0,fp/2]);
 grid
 title('Charakterystyka amplitudowa - skala logarytmiczna')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+xlabel('$\textsf{Czestotliwosc, }Hz$','Interpreter','latex')
+ylabel('$\textsf{Amplituda, } dB$', 'Interpreter','latex')
 %% b)
 
 
 %odpowiedź impulsowa dla NRZ
-h_nrz=ones(1,fp/Rs)/(fp/Rs);
+h_NRZ=1*ones(1,fp/Rs)/(fp/Rs); % -> bit
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %
 %  UZUPEŁNIĆ
-H_nrz=fft(h_nrz, N_fft);
-abs_H_nrz= abs(H_nrz);
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+H_NRZ=fft(h_NRZ, N_fft);
+abs_H_nrz= abs(H_NRZ);
 H_nrzdB= 20*log10(abs_H_nrz);
 figure (3)
 hold on
@@ -72,8 +79,6 @@ hold off
 xlim ([0,fp/2]);
 grid
 title('Charakterystyka amplitudowa - skala decybelowa')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %% c)
 
@@ -82,7 +87,7 @@ title('Charakterystyka amplitudowa - skala decybelowa')
 %  UZUPEŁNIĆ DLA POZOSTAŁYCH ALPHA
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+A=0.2; %współczynnik przekroczenia pasma ->
 sps=fp/Rs;
 span=M;
 shape='normal';
