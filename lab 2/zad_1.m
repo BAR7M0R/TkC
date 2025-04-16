1d46176ff1843855d806d522670dbaa53e18ea2a
Bartmor <bartmor.21@gmail.com>
2025-04-16 16:18:57 +0200

    uporządkowanie jednostek

--- lab 2/zad_1.m
+++ lab 2/zad_1.m
@@ -3,12 +3,13 @@
 clear; clc; close all;
 
 %% Dane przykładowe
-Rs=15;      %prędkość symbolowa -> bit/s
+Rs=30;      %prędkość symbolowa -> bit/s
 fp=120;      %próbkowanie -> Hz
 M=5;      %ilość odstępów międzysymbolowych -> bit 
 Ts = 1/Rs; % okres symbolowy -> s/bit
 N_fft=4096; %rozmiar FFT -> bezwymiarowe
-
+Tp = 1/fp;
+f_res = fp/N_fft;
 
 
 
@@ -17,13 +18,15 @@ N_fft=4096; %rozmiar FFT -> bezwymiarowe
 %% a) 	
 
 % określenie dziedziny czasu
-t=0:1/fp:M/Rs; % -> s
-% określenie dziedziny częstotliwości
-f=0:fp/N_fft:fp-1/N_fft; % -> Hz
+t=0:Tp:M/Rs; % -> s
 
+% określenie dziedziny częstotliwości
+f=0:f_res:fp-1/N_fft; % -> Hz
+t_h_sinc_max = M/(2*Rs);
 % wyznaczenie współczynników filtru
-h_sinc=1*Rs/fp*sinc(Rs*(t-M/(2*Rs))); % -> V or A
-sym_wst_gl= t(15) - t(7); % -> s
+h_amp = Rs/fp;
+h_sinc=h_amp*sinc(Rs*(t-M/(2*Rs))); % -> V or A #t-M/(2*Rs) != 0 -> t !=M/(2Rs)
+B= 1/(t(15) - t(7)); % -> s
 % wyświetlenie odpowiedzi impulsowej w dziedzinie czasu
 figure(1)
 stem(t,h_sinc)
@@ -32,10 +35,14 @@ title('Odpowiedź impulsowa')
 xlabel('$\textsf{Czas, }s$','Interpreter','latex')
 %xlabel('$s$','Interpreter','latex')
 ylabel('$\textsf{Amplituda, }V\textsf{ lub }A$', 'Interpreter','latex')
+xticks(0 : 2*Tp : M/Rs)
+labels_x = ["0", arrayfun(@(x) x + "Tp", 1:20)];
+labels_x = labels_x(1:2:end);
+xticklabels(labels_x)
+
 %wyznaczenie charakterystyki częstotliwościowej
 H_sinc=fft(h_sinc,N_fft); % -> V or A
 abs_H_sinc=abs(H_sinc); % -> V or A
-
 % wyświetlenie ch-ki amplitudowej - skala liniowa
 figure(2)
 plot(f,abs_H_sinc);
@@ -43,6 +50,11 @@ grid
 title('Charakterystyka amplitudowa - skala liniowa')
 xlabel('$\textsf{Czestotliwosc, }Hz$','Interpreter','latex')
 ylabel('$\textsf{Amplituda, }V\textsf{ lub }A$', 'Interpreter','latex')
+xticks(0:205*f_res:fp-1/N_fft)
+labels_x = ["0", arrayfun(@(x) x + "f_{rez}", 1:4095)];
+labels_x = labels_x(1:205:end);
+xticklabels(labels_x)
+xtickangle(90)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   UZUPEŁNIĆ O DECYBELOWĄ
@@ -57,11 +69,13 @@ grid
 title('Charakterystyka amplitudowa - skala logarytmiczna')
 xlabel('$\textsf{Czestotliwosc, }Hz$','Interpreter','latex')
 ylabel('$\textsf{Amplituda, } dB$', 'Interpreter','latex')
+xticks(0:125*f_res:fp-1/N_fft)
 %% b)
 
 
 %odpowiedź impulsowa dla NRZ
-h_NRZ=1*ones(1,fp/Rs)/(fp/Rs); % -> bit
+h_NRZ=1*ones(1,fp/Rs)/(fp/Rs); % -> V or A //zabieg który nie ma fizycznej interpr.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -72,7 +86,7 @@ h_NRZ=1*ones(1,fp/Rs)/(fp/Rs); % -> bit
 H_NRZ=fft(h_NRZ, N_fft);
 abs_H_nrz= abs(H_NRZ);
 H_nrzdB= 20*log10(abs_H_nrz);
-figure (3)
+figure (4)
 hold on
 plot(f, H_nrzdB);
 hold off
@@ -100,26 +114,29 @@ H_RC_30 = rcosdesign(1,span,sps,'normal');
 
 
 % wyświetlenie odpowiedzi impulsowej
-figure(4)
+figure(5)
 plot(t,H_RC_02,t,H_RC_05,t,H_RC_30);
 
 % wyznaczenie charakterystyk amplitudowych
 
 H_RC_02=fft(H_RC_02,N_fft);
 abs_H_RC_02 = abs(H_RC_02);
+H_abs_RC_02_dB = 20*log10(abs_H_RC_02);
 H_RC_05=fft(H_RC_05,N_fft);
 abs_H_RC_05 = abs(H_RC_05);
+H_abs_RC_05_dB = 20*log10(abs_H_RC_05);
 H_RC_30=fft(H_RC_30,N_fft);
 abs_H_RC_30 = abs(H_RC_30);
+H_abs_RC_30_dB = 20*log10(abs_H_RC_30);
 
 %wyświetlenie charakterystyk amplitudowych - skala liniowa i decybelowa
-figure(5)
+figure(6)
 plot(f,abs_H_RC_02,f,abs_H_RC_05,f,abs_H_RC_30);
 %xlim([0 4*Rs])
 xlim ([0 fp/2]);
 title("charakterystyka amplitudowa w skali liniowej")
 figure(6)
-plot(f,20*log10(abs_H_RC_02), f, 20*log10(abs_H_RC_05), f, 20*log10(abs_H_RC_30));
+plot(f,H_abs_RC_02_dB, f, H_abs_RC_05_dB, f, H_abs_RC_30_dB);
 %xlim([0 4*Rs])
 xlim ([0 fp/2]);
 title("charakterystyka amplitudowa w skali logarytmicznej")
